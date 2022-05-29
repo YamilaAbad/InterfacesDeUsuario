c7ecc5235215b8f03a6f6a413a1035b44d3b6aa6
M Victoria <mvittoria7@gmail.com>
2022-05-29 02:27:16 -0300

    mejoras

---
+++ TP2/assets/js/main.js
@@ -0,0 +1,21 @@
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader);
+
+
+
+/*==================== LINK ACTIVE ====================*/
+let linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
